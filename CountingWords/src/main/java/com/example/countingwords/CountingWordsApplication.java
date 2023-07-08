ca5299c4273033b664190f5e98d6e5d9ad30d154
rpjvanaert <ralf@van-aert.nl>
2023-07-08 11:26:59 +0200

    [ADD] Spring Boot project initialisation

---
+++ CountingWords/src/main/java/com/example/countingwords/CountingWordsApplication.java
@@ -0,0 +1,13 @@
+package com.example.countingwords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountingWordsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountingWordsApplication.class, args);
+    }
+
+}
---
+++ CountingWords/src/test/java/com/example/countingwords/CountingWordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.countingwords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountingWordsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
